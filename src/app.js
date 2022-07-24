21c1d01fe0dba8304578e36a70dde0bd213cba1f
KylymnykMaryna <kylymnykmaryna@gmail.com>
2022-07-24 14:50:42 +0300

    made api call

--- src/app.js
+++ src/app.js
@@ -19,7 +19,8 @@ let minutes =
 
 currentTime.innerHTML = `${day} ${hours}:${minutes}`;
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
@@ -49,6 +50,13 @@ function displayForecast() {
   console.log(forecastHTML);
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "c4808f82df6cb22aef0e17867c210521";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeatherCondition(response) {
   document.querySelector("#city-name").innerHTML = response.data.name;
   document.querySelector("#current-degrees").innerHTML = Math.round(
@@ -71,6 +79,7 @@ function displayWeatherCondition(response) {
     .setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemperature = response.data.main.temp;
+  getForecast(response.data.coord);
 }
 
 function search(city) {
